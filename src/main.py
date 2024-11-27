78154e37d71dc21270105aa970a0d7571240d3e6
mxlecomte <maxime.lecomte@inrae.fr>
2024-11-27 17:17:32 +0100

    main file for launching riptide

---
+++ src/main.py
@@ -0,0 +1,20 @@
+
+
+import argparse
+import os
+
+from riptide_sampling import get_flux_samples
+
+parser_general=argparse.ArgumentParser()
+
+parser_general.add_argument("-d", "--dose-level", help="enter the dose level. 'Control','Low','Middle','High'",type=str)
+parser_general.add_argument("-mol", "--molecule", help="enter the name of the molecule. amiodarone or valproic acid",type=str)
+
+
+args = parser_general.parse_args()
+
+if args.molecule and args.dose_level:
+    print(f"the {args.molecule} molecule is choosen.")
+    print(f"the {args.dose_level} dose level is choosen")
+
+    get_flux_samples('data/microarray/annotated_data_uniq_high_sd_flagged.tsv','data/microarray/open_tggates_cel_file_attribute.csv',sacrific_period='24 hr',dose_level=args.dose_level,compound_name=args.molecule)
