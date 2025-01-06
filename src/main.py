4917a9fcceafa877d1db58cd6964c6bccfa40a00
mxlecomte <maxime.lecomte@inrae.fr>
2025-01-06 10:12:46 +0100

    Riptide runs over data replicates

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ parser_general=argparse.ArgumentParser()
 
 parser_general.add_argument("-d", "--dose-level", help="enter the dose level. 'Control','Low','Middle','High'",type=str)
 parser_general.add_argument("-mol", "--molecule", help="enter the name of the molecule. amiodarone or valproic acid",type=str)
+parser_general.add_argument("-rep", "--replicates", help="number of replicates. By default, riptide take the min and max over all replicates.",type=int)
 
 
 args = parser_general.parse_args()
@@ -16,5 +17,6 @@ args = parser_general.parse_args()
 if args.molecule and args.dose_level:
     print(f"the {args.molecule} molecule is choosen.")
     print(f"the {args.dose_level} dose level is choosen")
+    print(f"the {args.replicates} replicates is choosen")
 
-    get_flux_samples('data/microarray/annotated_data_uniq_high_sd_flagged.tsv','data/microarray/open_tggates_cel_file_attribute.csv',sacrific_period='24 hr',dose_level=args.dose_level,compound_name=args.molecule)
+    get_flux_samples('data/microarray/annotated_data_uniq_high_sd_flagged.tsv','data/microarray/open_tggates_cel_file_attribute.csv',sacrific_period='24 hr',dose_level=args.dose_level,compound_name=args.molecule, replicates=args.replicates)
